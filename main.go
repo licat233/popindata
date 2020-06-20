a63c0aee8a41c56e5973be218ed9b2349d771bff
licat <1697470538@qq.com>
2020-06-20 16:51:33 +0800

    add Postpopindat()

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"popindata/popin"
 	"popindata/server"
 )
 
 func main() {
 	server.TimingMoney()
-	fmt.Println("start ...")
-	money := popin.Popin.GetAllMoneyPost("20200619")
-	fmt.Println(money)
 }
